feec40904ab340f6d179633d75fab709bab1b499
方家俊 <chris@fangjiajundeMacBook-Pro.local>
2020-03-28 10:10:53 +0800

    只有Spring空的框架

---
+++ gesture/src/test/java/com/interaction/gesture/GestureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interaction.gesture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
