3fb4107bcb87e79d05ef392eb5bd049bbca9c124
TrouBleMaKerV <782024062@qq.com>
2020-03-29 14:21:35 +0800

    添加注释

--- gesture/src/main/java/com/interaction/gesture/Entry/Project.java
+++ gesture/src/main/java/com/interaction/gesture/Entry/Project.java
@@ -1,10 +1,10 @@
 package com.interaction.gesture.Entry;
 
 public class Project {
-    String nameString;
-    String versionString;
-    String publisherString;
-    String uninstallPathString;
+    String nameString; //软件名称
+    String versionString; //版本号
+    String publisherString; // 出版商
+    String uninstallPathString; //路径
     public Project(String nameString, String versionString, String publisherString, String uninstallPathString) {
         this.nameString = nameString;
         this.versionString = versionString;
